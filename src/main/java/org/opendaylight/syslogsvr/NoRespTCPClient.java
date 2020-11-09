6085425c87bf742f91e26e243d0555cca44da1e1
spalagu <m.spalagu@gmail.com>
2020-11-09 10:58:03 +0800

    update

--- src/main/java/org/opendaylight/syslogsvr/NoRespTCPClient.java
+++ src/main/java/org/opendaylight/syslogsvr/NoRespTCPClient.java
@@ -2,15 +2,13 @@ package org.opendaylight.syslogsvr;
 
 import org.apache.jmeter.protocol.tcp.sampler.ReadException;
 import org.apache.jmeter.protocol.tcp.sampler.TCPClientImpl;
+import org.apache.jmeter.samplers.SampleResult;
 
 import java.io.InputStream;
 
-/**
- * Created by Wenbo Hu on 2016/1/5.
- */
 public class NoRespTCPClient extends TCPClientImpl {
-    @Override
-    public String read(InputStream is) throws ReadException {
+    public String read(InputStream is, SampleResult sampleResult) throws ReadException{
+        sampleResult.latencyEnd();
         return "";
     }
 }
